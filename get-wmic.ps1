7d3daed40704a271540364b730b78c48e908d3e6
petrho <petr.houzvik@elanor.cz>
2016-11-10 23:33:11 +0100

    Update get-wmic.ps1

--- get-wmic.ps1
+++ get-wmic.ps1
@@ -1,4 +1,6 @@
 Get-WmiObject -List
 Get-WmiObject -Class Win32_Bios
 
+Get-WmiObject -Class Win32_Service | ft
+
 (Get-WmiObject -Class Win32_Service -Filter "name='WinRM'" -ComputerName Server01).StopService()

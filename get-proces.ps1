1b7ca263d53a2f68342f3a0284010f37ae5c2b7a
petrho <petr.houzvik@elanor.cz>
2016-11-10 22:23:12 +0100

    Create get-proces.ps1

---
+++ get-proces.ps1
@@ -0,0 +1,10 @@
+#SYNTAX
+#    Get-Process [[-Name] <string[]>] [-ComputerName <string[]>] [-Module] [-FileVersionInfo]  [<CommonParameters>]
+#    Get-Process [[-Name] <string[]>] -IncludeUserName  [<CommonParameters>]
+#    Get-Process -Id <int[]> -IncludeUserName  [<CommonParameters>]
+#    Get-Process -Id <int[]> [-ComputerName <string[]>] [-Module] [-FileVersionInfo]  [<CommonParameters>]
+#    Get-Process -InputObject <Process[]> -IncludeUserName  [<CommonParameters>]
+#    Get-Process -InputObject <Process[]> [-ComputerName <string[]>] [-Module] [-FileVersionInfo]  [<CommonParameters>]
+
+get-Process TOTALCMD |select *
+get-Process  |select *
